a17773b52b2aac4616fc1f38b72561fc00f1a8a0
Adrian Neumann <aneumann@mpi-inf.mpg.de>
2011-10-27 14:36:26 +0200

    solver implementation

--- solver.py
+++ solver.py
@@ -1,5 +1,6 @@
 ﻿# -*- coding: utf-8 -*-
 import networkx as nx
+from networkx.algorithms.components.strongly_connected import strongly_connected_components
 
 def satisfiable(formula):
   #Build the implication graph
@@ -7,5 +8,18 @@ def satisfiable(formula):
   for (a,b) in formula.iterclauses():
 	G.add_edge(-a,b)
 	G.add_edge(-b,a)
+  
+  #check if a and -a in the same component
+  for component in strongly_connected_components(G):
+	seen = set()
+	for literal in component:
+	  v = abs(literal)
+	  if v in seen:
+		return False
+	  else:
+		seen.add(v)
+  
+  #components clean
+  return True
   
   
