4ad37cbd31642da3e7d65d4c045bc25e3f3ad3cf
Adrian Neumann <aneumann@mpi-inf.mpg.de>
2011-10-27 14:30:26 +0200

    initial commit for formulas

---
+++ formula.py
@@ -0,0 +1,31 @@
+﻿# -*- coding: utf-8 -*-
+
+from random import choice, randint
+
+class Formula(object):
+  """A 2-Sat Formula. Variables are integers, 0 is not allowed. Negative numbers are negated literals"""
+  def __init__(self):
+	self.f = []
+	
+  def add_clause(self,(a,b)):
+	f.append((a,b))
+  
+  def iterclause(self):
+	return iter(f)
+	
+  def num_clauses(self):
+	return len(self.f)
+	
+def uniform(c1,c2):
+  return choice((c1,c2))
+
+def random_clause(n):
+  sign = (-1,1)
+  cl = (choice(sign)*randint(1,n),choice(sign)*randint(1,n))
+  return cl
+  
+def random_formula(n, m, repeat = True, choice=uniform):
+  f = Formula()
+  for i in xrange(m):
+	f.add_clause(choice(random_clause(n), random_clause(n))
+  return f
---
+++ solver.py
@@ -0,0 +1,11 @@
+﻿# -*- coding: utf-8 -*-
+import networkx as nx
+
+def satisfiable(formula):
+  #Build the implication graph
+  G = nx.DiGraph()
+  for (a,b) in formula.iterclauses():
+	G.add_edge(-a,b)
+	G.add_edge(-b,a)
+  
+  
